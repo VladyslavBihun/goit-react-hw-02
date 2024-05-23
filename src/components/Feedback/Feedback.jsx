2cf2424576d07601c909b59b4eafe6993aee11b9
Vladyslav Bihun <bihunvv@gmail.com>
2024-05-23 19:04:44 +0300

    step 5 done

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -7,7 +7,7 @@ const Feedback = ({ values, totalFeedback }) => {
       <p>Neutral: {values.neutral}</p>
       <p>Bad: {values.bad}</p>
       <p>Total: {totalFeedback}</p>
-      {/* <p>Positive: </p> */}
+      <p>Positive: {Math.round((values.good / totalFeedback) * 100)}%</p>
     </div>
   );
 };
